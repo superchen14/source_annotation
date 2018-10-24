47cfd859c7a01a6b1e29ea7a8dce1b7b5095e8fc
Paul Chen <paul.chen03@sap.com>
2018-10-24 17:40:29 +0800

    add annotation for Provider.js

--- react-redux/components/Provider.js
+++ react-redux/components/Provider.js
@@ -2,6 +2,7 @@ import { Component, Children } from 'react'
 import PropTypes from 'prop-types'
 import { storeShape, subscriptionShape } from '../utils/PropTypes'
 
+// Provider组件的逻辑很简单，就是将redux的store（主要是getState以及dispatch接口）注入到context
 export function createProvider(storeKey = 'store') {
     const subscriptionKey = `${storeKey}Subscription`
 
@@ -12,7 +13,7 @@ export function createProvider(storeKey = 'store') {
 
         constructor(props, context) {
           super(props, context)
-          this[storeKey] = props.store;
+          this[storeKey] = props.store
         }
 
         render() {
@@ -24,6 +25,7 @@ export function createProvider(storeKey = 'store') {
         store: storeShape.isRequired,
         children: PropTypes.element.isRequired,
     }
+
     Provider.childContextTypes = {
         [storeKey]: storeShape.isRequired,
         [subscriptionKey]: subscriptionShape,
